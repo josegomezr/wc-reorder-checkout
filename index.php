4d411031d08857b6260c5b854eb0c520a1a800f2
Jose D. Gomez R <1josegomezr@gmail.com>
2019-05-08 15:05:53 -0400

    Reorder plantillas

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: Reorder checkout
+Description: Reordena la pagina de compras
+Author: José D. Gómez R.
+Author URI: https://josegomezr.com.ve
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+// Define WC_PLUGIN_FILE.
+if ( ! defined( 'WC_RC_PLUGIN_FILE' ) ) {
+	define( 'WC_RC_PLUGIN_FILE', __FILE__ );
+}
+
+function wc_rc_plugin_path() {
+	return untrailingslashit( plugin_dir_path( WC_RC_PLUGIN_FILE ) );
+}
+/**
+ * Reorder checkout page
+ */
+
+add_filter('wc_get_template', 'wc_rc_change_checkout_template', 10, 5);
+
+function wc_rc_change_checkout_template($template, $template_name, $args, $template_path, $default_path)
+{
+	if('checkout/form-checkout.php' === $template_name){
+		$template_name = 'templates/checkout-form.php';
+
+		$template_path = wc_rc_plugin_path() . '/templates/';
+		$default_path = wc_rc_plugin_path() . '/';
+		$template = wc_locate_template( $template_name, $template_path, $default_path );
+	}
+	return $template;
+}
+
+add_filter('wc_get_template', 'wc_rc_change_payment_template', 10, 5);
+
+function wc_rc_change_payment_template($template, $template_name, $args, $template_path, $default_path)
+{
+	if('checkout/payment.php' === $template_name){
+		$template_name = 'templates/payment.php';
+
+		$template_path = wc_rc_plugin_path() . '/templates/';
+		$default_path = wc_rc_plugin_path() . '/';
+
+		$template = wc_locate_template( $template_name, $template_path, $default_path );
+	}
+	return $template;
+}
